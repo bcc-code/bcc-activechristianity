28ac5eee4005d27e2bf100eb07324d7c7e69e067
Clement Wong <git@clement.hk>
2020-11-19 13:30:56 +0100

    Use justify-start/center/end instead of justify-between on nav

--- src/layout-parts/Nav/TopDesktop.tsx
+++ src/layout-parts/Nav/TopDesktop.tsx
@@ -14,11 +14,11 @@ const TopDesktop: React.FC<IDrawerNav & { explorePage?: INavItem }> = ({ isSideN
     return (
         <div className={`fixed top-0 z-50 bg-white hidden sm:block w-full py-1 border-b border-gray-200 drawer-main drawer-main-${isSideNavOpen ? 'open' : 'close'}`} >
             <TopFirst />
-            <div className="flex justify-between py-2">
-                <Link className='flex items-center px-4' to="/">
+            <div className="flex py-2">
+                <Link className='flex flex-1 justify-start items-center px-4' to="/">
                     <LogoFull full height="24px" />
                 </Link>
-                <div className="flex justify-center ">
+                <div className="flex flex-1 justify-center">
                     {menu.map((item, i) => (
                         <Link className="block p-2" key={i} to={`${item.to}`}>
                             {item.name}
@@ -26,7 +26,7 @@ const TopDesktop: React.FC<IDrawerNav & { explorePage?: INavItem }> = ({ isSideN
                     ))}
                 </div>
 
-                <div className="flex flex-row items-center">
+                <div className="flex flex-1 justify-end flex-row items-center">
                     {explorePage && (
                         <Link to={explorePage.to} className="px-2">
                             <Icon name="Search" size="6" />
