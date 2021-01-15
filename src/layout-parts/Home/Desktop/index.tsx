f4d0965ea90a368d2935f9e42e6260635883da6b
Bessie Wong <bessiewng@gmail.com>
2021-01-15 23:12:23 +0100

    test new mobile/desktop combination

--- src/layout-parts/Home/Desktop/index.tsx
+++ src/layout-parts/Home/Desktop/index.tsx
@@ -25,37 +25,20 @@ const HomeDesktop: React.FC<IHomeMobileProps> = ({ mixed, latest, popular, popul
         name: ac_strings.latest,
         slug: ac_strings.slug_latest
     }
+
     return (
 
         <div className="hiddem sm:block">
-            <DesktopFeaturedPostLoader loading={typeof mixed[0] === "undefined"}>
-                <HomeTopFeaturePost {...mixed[0]} key={shortid()} />
-            </DesktopFeaturedPostLoader>
-
             <div className="px-4">
-                <LatestSectionHeader latestSlug={latestPostAsTopic.slug} />
-                <LatestSection posts={latest.slice(0, 4)} />
                 <FeatureSectionDesktop
                     featuredPosts={mixed.slice(2)}
                 />
-                <Profiler id="Footer" onRender={(
-                    id, // the "id" prop of the Profiler tree that has just committed
-                    phase, // either "mount" (if the tree just mounted) or "update" (if it re-rendered)
-                    actualDuration, // time spent rendering the committed update
-                    baseDuration, // estimated time to render the entire subtree without memoization
-                    startTime, // when React began rendering this update
-                    commitTime, // when React committed this update
-                    interactions
-                ) => {
-                    console.log(id, phase, actualDuration, baseDuration, startTime, commitTime, interactions)
-                }}>
-                    <LowerSections
-                        lists={popularTopicsAll.static}
-                        newPostsForYou={[]}
-                        topicsForYou={popularTopicsAll.static}
-                        popularPosts={popular}
-                    />
-                </Profiler>
+                <LowerSections
+                    lists={popularTopicsAll.static}
+                    newPostsForYou={[]}
+                    topicsForYou={popularTopicsAll.static}
+                    popularPosts={popular}
+                />
 
                 <div className="grid grid-cols-4 gap-4 md:gap-6 sm:px-4">
                     <div className="col-start-1 col-end-3 lg:col-end-4">
--- src/templates/page/home.tsx
+++ src/templates/page/home.tsx
@@ -25,25 +25,24 @@ import { IPostItem, IPostRes, ITopicPostItems } from '@/types'
 // Helpers
 import ac_strings from '@/strings/ac_strings.js'
 
-const HomeContent: React.FC<IHomeProps> = (props) => {
-  const { pageContext, path } = props
-  const {
-    featuredPosts: featuredPosts,
-    popularTopics: popularTopicsAll,
-    popularPosts: popularPostsAll,
-    latestPosts: latestPosts
-
-  } = pageContext
-  const [isMobile, setIsMobile] = React.useState(typeof window !== "undefined" && window.innerWidth < 640)
-  const popularPosts = popularPostsAll.dynamic && popularPostsAll.dynamic.length > 0 ? popularPostsAll.dynamic : popularPostsAll.static
-  const { featured, latest, popular } = processRecommendationContext({ popularPosts, featuredPosts, latestPosts })
-
-  const mixedFeaturedPosts = getRandomFeatured({ latest, popular, featured })
-  const latestPostAsTopic = {
-    id: '',
-    name: ac_strings.latest,
-    slug: ac_strings.slug_latest
+const HomeContent: React.FC<{
+  mixed: IPostItem[]
+  latest: IPostItem[]
+  popular: IPostItem[]
+  popularTopicsAll: {
+    static: ITopicPostItems[]
+    dynamic: ITopicPostItems[]
   }
+}> = (props) => {
+
+  const {
+    mixed,
+    popularTopicsAll,
+    popular,
+    latest
+
+  } = props
+  const [isMobile, setIsMobile] = React.useState(typeof window !== "undefined" && window.innerWidth < 640)
 
   const checkIsMobile = () => {
     console.log('resizing')
@@ -59,58 +58,67 @@ const HomeContent: React.FC<IHomeProps> = (props) => {
   }, [
     typeof window !== "undefined" && window.innerWidth
   ])
-  if (isMobile) return (
-    <div className="sm:hidden">
-      <div className="w-full pb-4 pt-8">
-        <FeaturedBanner featured={mixedFeaturedPosts} />
-      </div>
-
-      <div className="div6 bg-gray-200 sm:bg-transparent py-6 overflow-hidden">
-        <PageSectionHeader title={ac_strings.latest} className="pb-4" />
-        <TopImgRowHorizontalScroll posts={latest} />
-      </div>
-
-      <div className="py-6">
-        <PageSectionHeader title={ac_strings.recommend_for_you} className="pb-4" />
-        <FeatureSectionMobile topicPosts={popularTopicsAll.static} />
-      </div>
-      <div className="py-6">
-        <PageSectionHeader title={ac_strings.topics_for_you} className="pb-4" />
-        <FeaturedTopics featured={popularTopicsAll.static} />
-
+  console.log(isMobile)
+  if (isMobile) {
+    return (
+      <div className="sm:hidden">
         <div className="div6 bg-gray-200 sm:bg-transparent py-6 overflow-hidden">
-          <PageSectionHeader title={ac_strings.popular} className="pb-4" />
-          <TopImgRowHorizontalScroll posts={popular} />
+          <PageSectionHeader title={ac_strings.latest} className="pb-4" />
+          <TopImgRowHorizontalScroll posts={latest} />
         </div>
 
-
-        <div className="w-full p-4 h-16">
-          <BgImgTopicCard
-            name={ac_strings.browse_resource}
-            to={ac_strings.slug_explore}
-          />
+        <div className="py-6">
+          <PageSectionHeader title={ac_strings.recommend_for_you} className="pb-4" />
+          <FeatureSectionMobile topicPosts={popularTopicsAll.static} />
         </div>
+        <div className="py-6">
+          <PageSectionHeader title={ac_strings.topics_for_you} className="pb-4" />
+          <FeaturedTopics featured={popularTopicsAll.static} />
+
+          <div className="div6 bg-gray-200 sm:bg-transparent py-6 overflow-hidden">
+            <PageSectionHeader title={ac_strings.popular} className="pb-4" />
+            <TopImgRowHorizontalScroll posts={popular} />
+          </div>
+
+
+          <div className="w-full p-4">
+            <div className="w-full h-16">
+              <BgImgTopicCard
+                name={ac_strings.browse_resource}
+                to={ac_strings.slug_explore}
+              />
+            </div>
+          </div>
+        </div>
+
       </div>
-
-    </div>
-  )
-  else {
+    )
+  } else {
 
     const DesktopHome = loadable(() => import("@/layout-parts/Home/Desktop"));
     return (
-      <DesktopHome
-        mixed={mixedFeaturedPosts}
-        latest={latest}
-        popular={popular}
-        popularTopicsAll={popularTopicsAll}
-      />
+      <DesktopHome {...props} />
     );
   };
 }
 const IndexPage: React.FC<IHomeProps> = (props) => {
   const { pageContext, path } = props
+  const {
+    featuredPosts: featuredPosts,
+    popularTopics: popularTopicsAll,
+    popularPosts: popularPostsAll,
+    latestPosts: latestPosts
 
+  } = pageContext
+  const popularPosts = popularPostsAll.dynamic && popularPostsAll.dynamic.length > 0 ? popularPostsAll.dynamic : popularPostsAll.static
+  const { featured, latest, popular } = processRecommendationContext({ popularPosts, featuredPosts, latestPosts })
 
+  const mixedFeaturedPosts = getRandomFeatured({ latest, popular, featured })
+  const latestPostAsTopic = {
+    id: '',
+    name: ac_strings.latest,
+    slug: ac_strings.slug_latest
+  }
   return (
 
     <div className="standard-max-w">
@@ -122,7 +130,25 @@ const IndexPage: React.FC<IHomeProps> = (props) => {
         breadcrumb={[]}
       />
 
-      <HomeContent {...props} />
+      <div className="sm:hidden w-full pb-4 pt-8">
+        <FeaturedBanner featured={mixedFeaturedPosts} />
+      </div>
+
+      <div className="hidden sm:block">
+        <DesktopFeaturedPostLoader loading={typeof mixedFeaturedPosts[0] === "undefined"}>
+          <HomeTopFeaturePost {...mixedFeaturedPosts[0]} key={shortid()} />
+        </DesktopFeaturedPostLoader>
+        <div className="px-4">
+          <LatestSectionHeader latestSlug={latestPostAsTopic.slug} />
+          <LatestSection posts={latest.slice(0, 4)} />
+        </div>
+      </div>
+      <HomeContent
+        mixed={mixedFeaturedPosts}
+        latest={latest}
+        popular={popular}
+        popularTopicsAll={popularTopicsAll}
+      />
     </div >
 
   )
