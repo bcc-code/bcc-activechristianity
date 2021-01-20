c8a47e6f03bba804709b7b6865efb7830ea8d732
Bessie Wong <bessiewng@gmail.com>
2021-01-20 06:06:56 +0100

    updated logos

--- src/images/ACLogo/index.tsx
+++ src/images/ACLogo/index.tsx
@@ -1,8 +1,18 @@
 import * as React from 'react'
 import loadable from '@loadable/component'
 
-const AcLogoEn = loadable(() => import('./en_US'))
-
+const En = loadable(() => import('./en_US'))
+const DE = loadable(() => import('./DE'))
+const ES = loadable(() => import('./ES'))
+const FI = loadable(() => import('./FI'))
+const FR = loadable(() => import('./FR'))
+const HU = loadable(() => import('./HU'))
+const IT = loadable(() => import('./IT'))
+const NL = loadable(() => import('./NL'))
+const NO = loadable(() => import('./NO'))
+const PL = loadable(() => import('./PL'))
+const PT = loadable(() => import('./PT'))
+const RU = loadable(() => import('./RU'))
 export interface ILogoStyle {
     height: string
     iconOnly?: boolean
@@ -13,7 +23,46 @@ export default (props: ILogoStyle) => {
     let lang = iconOnly ? "icon" : process.env.LANG_CODE
     const langLogo = {
         "en": (
-            <AcLogoEn {...props} />
+            <En {...props} />
+        ),
+        "de": (
+            <DE {...props} />
+        ),
+        "da": (
+            <NO {...props} />
+        ),
+        "es": (
+            <ES {...props} />
+        ),
+        "fi": (
+            <FI {...props} />
+        ),
+        "fr": (
+            <FR {...props} />
+        ),
+        "it": (
+            <IT {...props} />
+        ),
+        "hu": (
+            <HU {...props} />
+        ),
+        "nb": (
+            <NO {...props} />
+        ),
+        "nl": (
+            <NL {...props} />
+        ),
+        "pt": (
+            <PT {...props} />
+        ),
+        "pl": (
+            <PL {...props} />
+        ),
+        "ru": (
+            <RU {...props} />
+        ),
+        "se": (
+            <NO {...props} />
         )
     }
 
