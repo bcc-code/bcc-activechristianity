b648a5f091748f9dcce73937115c2a5213837c17
Bessie Wong <bessiewng@gmail.com>
2021-02-05 05:29:53 +0100

    added middleware

--- src/rootElement.js
+++ src/rootElement.js
@@ -3,8 +3,10 @@
 import React from "react"
 import {languages} from './strings/generated/menus.json'
 import { Provider } from "react-redux"
-import { createStore, applyMiddleware } from "redux"
-import reducers from "./src/state/reducer";
+import { createStore, applyMiddleware} from "redux"
+import authApi from './src/state/middleware/auth'
+import userApi from './src/state/middleware/user'
+import reducers from "./state/reducer";
 export const preloadedState = {
     auth: {
         loggedIn: 'notLoggedIn'
@@ -39,7 +41,7 @@ import "./styles/reset.css"
 import "./styles/tailwind-output.css"
 
 export default ({ element }) => {
-    
+    const store = createStore(reducers, preloadedState,applyMiddleware(authApi,userApi ))
     return (
         <Provider store={store}>
                 <div>
