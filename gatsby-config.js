4547545e75f272049eb19eae57118178be9c7710
Bessie Wong <bessiewng@gmail.com>
2021-02-05 10:52:56 +0100

    disabled preact

--- gatsby-config.js
+++ gatsby-config.js
@@ -121,9 +121,9 @@ if (activeEnv === 'production') {
     {
       resolve: 'gatsby-plugin-sitemap'
     },
-    {
+/*     {
       resolve:'gatsby-plugin-preact'
-    }
+    } */
   )
 
   if( process.env.NO_FOLLOW==="true"){
