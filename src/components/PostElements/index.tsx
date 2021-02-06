b571f201e1b033509bb634fb3291c856f3771305
Bessie Wong <bessiewng@gmail.com>
2021-02-06 23:20:49 +0100

    fixed slug error

--- src/components/PostElements/index.tsx
+++ src/components/PostElements/index.tsx
@@ -30,7 +30,7 @@ export const PostLabel: React.FC<{ text: string | JSX.Element }> = ({ text }) =>
 
 export const AuthorLink: React.FC<{ authorGroups?: IPostAuthors[] }> = ({ authorGroups }) => {
     return <span>{authorGroups && authorGroups[0] && authorGroups[0].authors ?
-        authorGroups[0].authors.map((item, k) => <Link key={k} className="inline-block post-meta-commar" to={`${pages.author.to}/${item.to}`}>{item.name}</Link>) : ''}</span>
+        authorGroups[0].authors.map((item, k) => <Link key={k} className="inline-block post-meta-commar" to={`${ac_strings.slug_author}/${item.to}`}>{item.name}</Link>) : ''}</span>
 }
 export const ReadingTimingAuthor: React.FC<IProps> = ({ duration, authors, className }) => {
 
