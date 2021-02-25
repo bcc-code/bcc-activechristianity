1de9d1570f6c833e7f1ee4029b895a11348e2615
Bessie Wong <bessiewng@gmail.com>
2021-02-25 17:44:25 +0100

    updated api endpoint

--- src/strings/static/endpoints.js
+++ src/strings/static/endpoints.js
@@ -1,17 +1,17 @@
 module.exports = {
-    "ebooks":"https://new.acadmin.net/ebooks",
+    "ebooks":"https://acadmin.net/ebooks",
     "contact_hero":"https://media.activechristianity.org/2019/08/ac-home-hero-bg.jpg",
     "podcast_logo":"https://media.activechristianity.org/2020/08/living-the-gospel-season-2-logo-compress.jpg",
-    "facebook_login_redirect":"https://new.acadmin.net/social/facebook/redirect",
+    "facebook_login_redirect":"https://acadmin.net/social/facebook/redirect",
     "contact_form_api":"https://q4jq50xsab.execute-api.us-east-1.amazonaws.com/prod",
     "dummy_image_api":"https://dummyimage.com",
     "about_us_video":"https://www.youtube.com/embed/sAbW36EzKIc",
     "reftagger":"https://api.reftagger.com/v2/RefTagger.js",
-    "api_host":"https://new.acadmin.net",
-    "api_url":"https://new.acadmin.net/graphql",
+    "api_host":"https://acadmin.net",
+    "api_url":"https://acadmin.net/graphql",
     "translation_google_sheet_auth":"AIzaSyASDGn4oZErww-RsW6kBPnQz7tCB6x3Fpk",
     "translation_google_sheet_id":"1waay0ca8OdtmJFyMtKzOsje_gocnWwfI2ZUFh5Bg90U",
     "test":"test",
-    "slug_translation":"https://new.acadmin.net/day4/string_translations"
+    "slug_translation":"https://acadmin.net/day4/string_translations"
 }
 
