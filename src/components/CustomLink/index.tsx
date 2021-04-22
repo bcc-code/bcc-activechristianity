89d6d5897dbfe4c069217ce09ce515e6913290f0
Bessie Wong <bessiewng@gmail.com>
2021-04-22 07:07:20 +0200

    test Link from reach/router

--- src/components/CustomLink/index.tsx
+++ src/components/CustomLink/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { GatsbyLinkProps } from 'gatsby'
 import { trimSlug } from '@/helpers/index-js'
+import { Link } from "@reach/router"
 type Omit<T, K> = Pick<T, Exclude<keyof T, K>>;
 
 const CustomLink: React.FC<Omit<GatsbyLinkProps<{}>, 'ref'>> = ({ to, ...rest }) => {
     let updatedTo = to ? trimSlug(to) : ''
 
-    return <a href={`/${updatedTo}`} {...rest} />
+    return <Link to={`/${updatedTo}`} {...rest} />
 }
 
 export default CustomLink
