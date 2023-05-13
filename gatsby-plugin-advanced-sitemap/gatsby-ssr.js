eb7d01a0005849323b1af8b3ebe906f52b4241ea
Gerard <gerardlamo@gmail.com>
2023-05-13 09:16:59 +0200

    Manual merged changes

--- gatsby-plugin-advanced-sitemap/gatsby-ssr.js
+++ gatsby-plugin-advanced-sitemap/gatsby-ssr.js
@@ -1,30 +1,21 @@
 "use strict";
 
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
-
 var _extends2 = _interopRequireDefault(require("@babel/runtime/helpers/extends"));
-
 var _react = _interopRequireDefault(require("react"));
-
 var _gatsby = require("gatsby");
-
 var _defaults = _interopRequireDefault(require("./defaults"));
-
 exports.onRenderBody = function (_ref, pluginOptions) {
   var setHeadComponents = _ref.setHeadComponents;
-
   var _defaultOptions$plugi = (0, _extends2.default)({}, _defaults.default, pluginOptions),
-      output = _defaultOptions$plugi.output,
-      createLinkInHead = _defaultOptions$plugi.createLinkInHead;
-
+    output = _defaultOptions$plugi.output,
+    createLinkInHead = _defaultOptions$plugi.createLinkInHead;
   if (!createLinkInHead) {
     return;
   }
-
   if (output.charAt(0) !== "/") {
     output = "/" + output;
   }
-
   setHeadComponents([/*#__PURE__*/_react.default.createElement("link", {
     key: "gatsby-plugin-advanced-sitemap",
     rel: "sitemap",
